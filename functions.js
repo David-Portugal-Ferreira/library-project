42cba102faa3ed253ebdb2b416d043bb0182705b
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-08-19 09:53:14 +0100

    Add functionality to change read status of a book

--- functions.js
+++ functions.js
@@ -1,19 +1,4 @@
-const myLibrary = [
-    {
-        title: 'Test',
-        author: 'test',
-        volume: 1,
-        pages: 300,
-        readStatus: false,
-    },
-    {
-        title: 'Test_2',
-        author: 'test_2',
-        volume: 1,
-        pages: 200,
-        readStatus: true,
-    },
-];
+const myLibrary = [];
 
 function Book(title, author, volume, pages, readStatus) {
     this.title = title;
@@ -21,6 +6,14 @@ function Book(title, author, volume, pages, readStatus) {
     this.volume = volume;
     this.pages = pages;
     this.readStatus = readStatus;
+}
+Book.prototype.changeReadStatus = function () {
+    this.readStatus = !this.readStatus;
+}
+
+if (myLibrary.length === 0) {
+    const book = new Book('Test', 'test', 1, 300, false);
+    addBookToLibrary(book);
 }
 
 function addBookToLibrary(book) {
@@ -33,7 +26,7 @@ const submitForm = document.querySelector('input[type=submit]');
 const dialog = document.querySelector('dialog');
 const closeButton = document.querySelector('dialog button');
 
-btnAddBook.addEventListener('click', () => {dialog.showModal()});
+btnAddBook.addEventListener('click', () => { dialog.showModal() });
 closeButton.addEventListener('click', () => dialog.close());
 
 submitForm.addEventListener('click', (e) => {
@@ -54,38 +47,53 @@ submitForm.addEventListener('click', (e) => {
 })
 
 function loadBooks() {
-    if(libraryBooks.hasChildNodes()) {
-        libraryBooks.replaceChildren();
-    }
-    myLibrary.map((book) => {
-        const card = document.createElement('div');
-        card.classList = 'card';
-        let title = document.createElement('h2');
-        title.innerText = `Title: ${book.title}`;
-        card.appendChild(title);
-        let author = document.createElement('h3');
-        author.innerText = `Author: ${book.author}`;
-        card.appendChild(author);
-        let volumePhara = document.createElement('p');
-        volumePhara.innerText = `Volume: ${book.volume}`;
-        card.appendChild(volumePhara);
-        let pagesPhara = document.createElement('p');
-        pagesPhara.innerText = `Nº Pages ${book.pages}`;
-        card.appendChild(pagesPhara);
-        let readStatusPhara = document.createElement('p');
-        readStatusPhara.innerText = book.readStatus ? 'Status: Completed' : 'Status: To Read';;
-        card.appendChild(readStatusPhara);
-
-        const removeButton = document.createElement('button');
-        removeButton.addEventListener('click', () => {
-            let bookIndex = myLibrary.indexOf(book);
-            myLibrary.splice(bookIndex, 1);
-            loadBooks();
-        })
-
-        card.appendChild(removeButton)
-
-        libraryBooks.appendChild(card);
-    })
+    reloadLibraryBooks();
+    myLibrary.map(book => constructCard(book));
 }
 loadBooks();
+
+function constructCard(book) {
+    const card = document.createElement('div');
+    card.classList = 'card';
+    let title = document.createElement('h2');
+    title.innerText = `Title: ${book.title}`;
+    card.appendChild(title);
+    let author = document.createElement('h3');
+    author.innerText = `Author: ${book.author}`;
+    card.appendChild(author);
+    let volumePhara = document.createElement('p');
+    volumePhara.innerText = `Volume: ${book.volume}`;
+    card.appendChild(volumePhara);
+    let pagesPhara = document.createElement('p');
+    pagesPhara.innerText = `Nº Pages ${book.pages}`;
+    card.appendChild(pagesPhara);
+
+    let readStatusPhara = document.createElement('p');
+    readStatusPhara.innerText = book.readStatus ? 'Status: Completed' : 'Status: To Read';
+    card.appendChild(readStatusPhara);
+    const changerReadStatus = document.createElement('input');
+    changerReadStatus.setAttribute("type", "checkbox")
+    changerReadStatus.addEventListener('click', () => {
+        book.changeReadStatus();
+        readStatusPhara.innerText = book.readStatus ? 'Status: Completed' : 'Status: To Read';
+    })
+    card.appendChild(changerReadStatus);
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove Book';
+    removeButton.addEventListener('click', () => {
+        let bookIndex = myLibrary.indexOf(book);
+        myLibrary.splice(bookIndex, 1);
+        loadBooks();
+    })
+    card.appendChild(removeButton)
+
+    
+    libraryBooks.appendChild(card);
+}
+
+function reloadLibraryBooks() {
+    if (libraryBooks.hasChildNodes()) {
+        libraryBooks.replaceChildren();
+    }
+}
